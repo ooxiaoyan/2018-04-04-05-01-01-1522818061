203abbd18bc62ea3b52e4a5b302be180debc30a8
ooxiaoyan <810978527@qq.com>
2018-04-20 14:19:33 +0800

    commit RandomIntGeneratorTest.java

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,37 @@
 package tw.core;
 
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
 
+    private RandomIntGenerator randomIntGenerator;
+    private InputValidator inputValidator;
+
+    @Before
+    public void setup() {
+        randomIntGenerator = new RandomIntGenerator();
+        inputValidator = new InputValidator();
+    }
+
+    @Test
+    public void should_return_four_distinct_int_less_than_9() {
+        String result = randomIntGenerator.generateNums(9,4);
+        assertTrue(inputValidator.validate(result));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_return_IllegalArgumentException_when_digitmax_greater_than_numbersOfNeed() {
+        String exception = "Can't ask for more numbers than are available";
+        System.out.println(exception);
+        randomIntGenerator.generateNums(3,4);
+    }
 }
