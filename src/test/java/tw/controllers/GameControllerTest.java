1608ee699827720d3c9bdcfd28eec2c46f200e1a
ooxiaoyan <810978527@qq.com>
2018-04-21 22:35:59 +0800

    commit GameControllerTest.java

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,54 @@
 package tw.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tw.commands.InputCommand;
+import tw.core.Answer;
+import tw.core.Game;
+import tw.views.GameView;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
 
+    private GameController gameController;
+
+    @Mock
+    private Game game;
+    @Mock
+    private GameView gameView;
+    @Mock
+    private InputCommand inputCommand;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test_beginGame() throws IOException {
+        gameController = new GameController(game, gameView);
+        gameController.beginGame();
+        verify(gameView).showBegin();
+    }
+
+    @Test
+    public void test_play_show_results_when_checkCoutinue_is_false() throws IOException {
+        when(inputCommand.input()).thenReturn(Answer.createAnswer("1 6 3 4"));
+        when(game.checkCoutinue()).thenReturn(false);
+        when(game.checkStatus()).thenReturn("fail");
+        GameController gameController = new GameController(game, gameView);
+        gameController.play(inputCommand);
+        verify(gameView).showGameStatus(anyString());
+    }
 }
