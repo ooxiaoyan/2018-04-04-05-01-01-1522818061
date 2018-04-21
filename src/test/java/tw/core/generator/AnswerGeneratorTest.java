1dd077123dd264e63fb665700a1347868c4ad631
ooxiaoyan <810978527@qq.com>
2018-04-21 22:53:48 +0800

    Add throw exception test case in AnswerGeneratorTest.java

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -16,18 +16,26 @@ public class AnswerGeneratorTest {
 
     private AnswerGenerator answerGenerator;
     private String answerString = "1 2 3 4";
+    private RandomIntGenerator randomIntGenerator;
 
     @Before
     public void setup() {
-        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
-        when(randomIntGenerator.generateNums(9, 4)).thenReturn(answerString);
-        answerGenerator = new AnswerGenerator(randomIntGenerator);
+        randomIntGenerator = mock(RandomIntGenerator.class);
     }
 
     @Test
-    public void should_return_Answer_object() throws OutOfRangeAnswerException {
+    public void test_generate_should_return_Answer_object() throws OutOfRangeAnswerException {
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn(answerString);
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
         Answer result = answerGenerator.generate();
         assertNotNull(result);
+    }
+
+    @Test(expected = OutOfRangeAnswerException.class)
+    public void test_generate_should_throw_OutOfRangeAnswerException_when_the_number_is_not_between_0_and_9() throws OutOfRangeAnswerException {
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("3 15 3 9");
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
+        answerGenerator.generate();
     }
 }
 
